f9dd9a4103a19d355c9d7a294f7040cd739773c0
jahnluuu <131157302+jahnluuu@users.noreply.github.com>
2024-11-13 14:53:33 +0800

    Delete ElectricityConsumptionBilling directory

--- ElectricityConsumptionBilling/ECBApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Tariff, Consumption, Bill, BillingDetails, Payment
-
-admin.site.register(Customer)
-admin.site.register(Tariff)
-admin.site.register(Consumption)
-admin.site.register(Bill)
-admin.site.register(BillingDetails)
-admin.site.register(Payment)
